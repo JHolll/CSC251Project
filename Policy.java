c823d71b7f6436b6afb3a2f3fb5158d607df92cc
JHolll <97764451+JHolll@users.noreply.github.com>
2023-10-01 22:19:18 -0400

    Update Policy.java
    Added documentation comments

--- Policy.java
+++ Policy.java
@@ -29,7 +29,16 @@ public class Policy
       bMI = -1;
       price = -1;
    }
-   
+   /**
+      @param pnumber policy number
+      @param provider provider name
+      @param firstName user's first name
+      @param lastName user's last name
+      @param age user's age
+      @param smoking if the user smokes
+      @param height user's height
+      @param weight user's weight
+   */
       public Policy(int policyNum, String providerName, String pHFirstName, String pHLastName, int pHAge, String pHSmoking, double pHHeight, double pHWeight)
    {
       pNumber = policyNum;
@@ -42,53 +51,84 @@ public class Policy
       weight = pHWeight;
    }
    
-      
+   /**
+      @return policy number
+   */
    public int setPolicyNumber()
    {
       return pNumber;
    }
    
+   /**
+      @return provider name
+   */
    public String setProviderName()
    {
       return provider;
    }
    
+   /**
+      @return the user's first name
+   */
    public String setPHFirstName()
    {
       return firstName;
    }
    
+   /**
+      @return the user's last name
+   */
    public String setPHLastName()
    {
       return lastName;
    }
-
+   
+   /**
+      @return the user's age
+   */
    public int setPHAge()
    {
       return age;
    }
-
+   
+   /**
+      @return whether the user smokes
+   */
    public String setPHSmoking()
    {
       return smoking;
    }
    
+   /**
+      @return the user's height
+   */
    public double setPHHeight()
    {
       return height;
    }
    
+   /**
+      @return the user's weight
+   */
    public double setPHWeight()
    {
       return weight;
    }
    
+   /**
+      @param bMI users BMI
+      @return the user's BMI
+   */
    public double GetBMI()
    {
       bMI = (weight * 703) / (height * height);
       return bMI;
    }
+   /**
    
+   @param price 
+   @return the price of the policy
+   */
    public double GetPrice()
    {
       price = 600;
